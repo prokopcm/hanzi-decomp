c8f986b137527db1680dc0b90c4b5dcca625a446
Michael Prokopchuk <prokopcm@gmail.com>
2020-02-18 15:07:02 -0500

    Update decompose to use decomp map data, update unit test

--- src/__test__/index.test.ts
+++ src/__test__/index.test.ts
@@ -1,5 +1,21 @@
 import { decompose } from '../index'
 
 test('Initial test', () => {
-  expect(decompose('你')).toEqual(['你'])
+  const decompList = decompose('你')
+  const expectedDecomp = {
+    components: ['亻', '尔'],
+    type: '⿰',
+    writing: '你'
+  }
+
+  expect(decompList).toHaveProperty('0')
+
+  const decomp = decompList[0]
+  expect(decomp).toHaveProperty('components')
+  expect(decomp).toHaveProperty('type')
+  expect(decomp).toHaveProperty('writing')
+  expect(decomp.components.length).toBeGreaterThanOrEqual(0)
+  expect(typeof decomp.components.length).toBe('number')
+  expect(decomp.type).toBe(expectedDecomp.type)
+  expect(decomp.writing).toBe(expectedDecomp.writing)
 })
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
+import decompMap from './data/decompMap'
+
 /**
  * Breaks apart a string of characters into their component parts.
  * @param characters a string of characters to decompose
  * @return an array of each character broken down into its component parts
  */
 export function decompose (characters: string): any {
-  const trimmedCharacters = characters.replace(/\s/g, '')
-  const characterList = Array.from(trimmedCharacters)
+  const characterList = []
+  const trimmedCharacters = Array.from(characters.replace(/\s/g, ''))
+
+  for (const zi of trimmedCharacters) {
+    if ((decompMap as any)[zi]) {
+      characterList.push((decompMap as any)[zi])
+    }
+  }
 
   return characterList
 }
