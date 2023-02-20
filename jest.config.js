a5e753772a0fb689a957d79baeb71d18461a2ab9
Michael Prokopchuk <prokopcm@gmail.com>
2023-02-20 11:08:02 -0500

    Fix test ignore path to exclude lib directory

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.ts?$': 'ts-jest'
   },
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   moduleFileExtensions: ['ts', 'js', 'json', 'node']
 }
